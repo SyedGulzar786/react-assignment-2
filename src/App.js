b6a53339794dee173e1205b6a6d94763b94cec43
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-07 08:16:39 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {
+          console.log("hello world.!")
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
